5d0ceba0ef17d850e7e265342fc67efd999a9b66
Eugene <contact.duk@gmail.com>
2022-02-21 13:58:58 +0300

    Adding initial source code

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Eugene on 17.01.22.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
